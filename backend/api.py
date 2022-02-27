157e1e7154c8c1eccb6667aab43b0e707bd30520
Sam Schoedel <sschoedel@vt.edu>
2022-02-27 06:28:13 -0500

    instagram properly filters 6 total images

--- backend/api.py
+++ backend/api.py
@@ -3,6 +3,13 @@ from flask_cors import CORS, cross_origin
 import honestuser
 import json
 import backend
+from flask import Flask, request, jsonify
+import json
+import cv2
+import pymongo
+from flask_cors import CORS, cross_origin
+import numpy as np
+from bson.json_util import dumps
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
@@ -23,15 +30,17 @@ def createUser(userID, firstname, lastname, twitterHandle, instaUsername, instaP
     return json.dumps(usr)
 
 @app.route('/scrapeall', methods=['POST'])
+@cross_origin()
 def scrapeAll():
-    # userID, firstname, lastname, twitterHandle, instaUsername, instaPassword):
     firstname = request.json["firstname"]
     lastname = request.json["lastname"]
     twitterHandle = request.json["twitterhandle"]
     instaUsername = request.json["instauser"]
     instaPassword = request.json["instapass"]
+    print(f"twithandle: {twitterHandle}")
     usr = honestuser.HonestUser(0, firstName=firstname, lastName=lastname, twitterUser=twitterHandle, instaUser=instaUsername, instaPass=instaPassword)
     backend.setup_connection('../connection.txt')
+    backend.create_user(usr.firstName, usr.lastName, "", 25)
     
     # Twitter
     twitInfo = usr.getTwitterInfo()
@@ -41,20 +50,29 @@ def scrapeAll():
     backend.get_all_twitter_info(usr.firstName, usr.lastName)
 
     # Instagram
-    usr.getInstaInfo()
+    instaInfo = usr.getInstaInfo()
+    print(f"instaInfo: {instaInfo}")
+    backend.add_pfp_to_user(usr.firstName, usr.lastName, instaInfo[0]['url'])
+
     backend.create_user_instagram_info(usr.firstName, usr.lastName, usr.instaUser)
+    if len(instaInfo) > 1:
+        for image in instaInfo[1:]:
+            print("adding image")
+            backend.add_image_to_user(usr.firstName, usr.lastName, image['url'])
+    
 
     # Spotify
     spotInfo = usr.getSpotifyInfo()
-    backend.create_spotify_info(usr.firstName, usr.lastName, usr.spotifyUsername)
+    backend.create_spotify_info(usr.firstName, usr.lastName, usr.spotifyUsername, spotInfo['topGenres'])
     for recentSong in spotInfo['topSongs']:
         backend.add_top_recent_songs(usr.firstName, usr.lastName, recentSong['songname'], recentSong['album_cover'], spotInfo['avgSongEnergy'])
     for topArtist in spotInfo['topArtists']:
         backend.add_top_artists(usr.firstName, usr.lastName, topArtist['artistname'], topArtist['imageurl'])
     topSongAllTime = spotInfo['topSongAllTime']
-    backend.add_favorite_song(usr.firstName, usr.lastName, topSongAllTime['songname'], topSongAllTime['imageurl'])
-
-    return backend.get_all_user_info(usr.firstName, usr.lastName)
+    backend.add_favorite_song(usr.firstName, usr.lastName, topSongAllTime['songname'], topSongAllTime['album_cover'])
+    data = backend.get_all_user_info(usr.firstName, usr.lastName)
+    # print(f"backend data: {data}")
+    return data
 
 @app.route('/scrapetwitter', methods=['POST'])
 def scrapeTwit(userID):
